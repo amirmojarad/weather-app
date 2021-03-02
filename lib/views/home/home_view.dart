820e22731b48c2a490575b8b3f2f32fcfc14d02d
amir mojarad <amirmojarad13@gmail.com>
2021-03-02 12:02:46 +0330

    added some widgets and fl charts components

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:weather/models/api/weather.dart';
 import 'package:weather/models/utils/device.dart';
@@ -6,6 +7,7 @@ import 'package:weather/views/home/widgets/sections/humidity.dart';
 import 'package:weather/views/home/widgets/sections/pressure.dart';
 import 'package:weather/views/home/widgets/sections/wind_speed.dart';
 import 'package:weather/views/home/widgets/upper_widgets.dart';
+import 'file:///D:/applications/AndroidProjects/weather_app/weather/lib/models/localizations/app_localizations.dart';
 
 import 'widgets/sections/info_tags.dart';
 import 'widgets/sections/sunrise_sunset.dart';
@@ -41,7 +43,7 @@ class _HomeViewState extends State<HomeView> {
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 18.0, bottom: 10),
+                padding: const EdgeInsets.only(top: 32.0, bottom: 32),
                 child: AspectRatio(
                   aspectRatio: 5,
                   child: Center(
@@ -52,7 +54,7 @@ class _HomeViewState extends State<HomeView> {
               Padding(
                 padding: const EdgeInsets.all(15.0),
                 child: AspectRatio(
-                  aspectRatio: 2.5,
+                  aspectRatio: 2.2,
                   child: Center(
                     child: buildCardInfo(
                       widget.weather.current.temp,
@@ -121,17 +123,20 @@ class _HomeViewState extends State<HomeView> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.only(
+                    top: 16.0, bottom: 32, left: 10, right: 10),
                 child: AspectRatio(
                   aspectRatio: 2,
                   child: Container(
+                    child: WeekChart(),
                     decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(8),
                     ),
                   ),
                 ),
-              )
+              ),
+              SizedBox(height: 250)
             ],
           ),
         ),
@@ -142,6 +147,126 @@ class _HomeViewState extends State<HomeView> {
             colors: [Color(0xffE7FFFA), Color(0xffF1FFFC)],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class WeekChart extends StatelessWidget {
+  List<Color> gradientColors = [
+    const Color(0xff23b6e6),
+    const Color(0xff02d39a),
+  ];
+
+  WeekChart({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LineChart(
+      LineChartData(
+        gridData: FlGridData(
+          show: false,
+          drawVerticalLine: false,
+          getDrawingHorizontalLine: (value) {
+            return FlLine(
+              color: const Color(0xff37434d),
+              strokeWidth: 1,
+            );
+          },
+        ),
+        titlesData: FlTitlesData(
+          show: true,
+          bottomTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 22,
+            getTextStyles: (value) => const TextStyle(
+              color: Color(0xff68737d),
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+            getTitles: (value) {
+              switch (value.toInt()) {
+                case 1:
+                  return AppLocalizations.of(context).translate("sat");
+                case 2:
+                  return 'Mon';
+                case 3:
+                  return 'Tue';
+                case 4:
+                  return 'Wed';
+                case 5:
+                  return 'Thu';
+                case 6:
+                  return 'Sat';
+                case 7:
+                  return "Fri";
+              }
+              return '';
+            },
+            margin: 8,
+          ),
+          leftTitles: SideTitles(
+            showTitles: true,
+            getTextStyles: (value) => const TextStyle(
+              color: Color(0xff67727d),
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+            getTitles: (value) {
+              switch (value.toInt()) {
+                case 10:
+                  return '10';
+                case 20:
+                  return '20';
+                case 30:
+                  return '30';
+                case 40:
+                  return '40';
+                case 50:
+                  return '50';
+              }
+              return '';
+            },
+            reservedSize: 28,
+            margin: 12,
+          ),
+        ),
+        borderData: FlBorderData(
+          show: true,
+          border: Border.all(color: const Color(0xff37434d), width: 1),
+        ),
+        minX: 0,
+        maxX: 8,
+        minY: 0,
+        maxY: 50,
+        lineBarsData: [
+          LineChartBarData(
+            spots: [
+              FlSpot(1, 1),
+              FlSpot(2, 2),
+              FlSpot(3, 3),
+              FlSpot(4, 50),
+              FlSpot(5, 5),
+              FlSpot(6, 7),
+              FlSpot(7, 20),
+            ],
+            isCurved: true,
+            colors: gradientColors,
+            barWidth: 5,
+            isStrokeCapRound: true,
+            dotData: FlDotData(
+              show: true,
+            ),
+            belowBarData: BarAreaData(
+              show: true,
+              colors: gradientColors
+                  .map((color) => color.withOpacity(0.3))
+                  .toList(),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/views/home/widgets/card_info.dart
+++ lib/views/home/widgets/card_info.dart
@@ -3,36 +3,76 @@ import 'package:weather/models/utils/device.dart';
 import 'package:weather/view_models/api_handler/api_utils.dart';
 import 'file:///D:/applications/AndroidProjects/weather_app/weather/lib/models/localizations/app_localizations.dart';
 
-Container buildCardInfo(
-    dynamic temp, dynamic feelsLike, String icon, String description, BuildContext context) {
+Container buildCardInfo(dynamic temp, dynamic feelsLike, String icon,
+    String description, BuildContext context) {
   return Container(
     child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GridView.count(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Image.network(
+                getIcons(icon),
+                color: Colors.white,
+              ),
+              Text(
+                "$description",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: device.width - 300,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 6.0),
+            child:
+                buildLeftSectionCard(temp, feelsLike, context),
+          ),
+          // Column(
+          //   children: [
+          //     Image.network(
+          //       getIcons(icon),
+          //       color: Colors.white,
+          //     ),
+          //     Text(
+          //       "$description",
+          //       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          //     ),
+          //   ],
+          // ),
+        ],
+      ),
+
+      /*GridView.count(
         physics: NeverScrollableScrollPhysics(),
         crossAxisCount: 2,
         mainAxisSpacing: 1,
         shrinkWrap: true,
         children: [
           buildLeftSectionCard(temp, feelsLike, context),
-          Column(
-            children: [
-              Image.network(
-                getIcons(icon),
-                color: Colors.yellow,
-                width: device.width / 4.5,
-              ),
-              Text(
-                "$description",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 48.0),
+            child: Column(
+              children: [
+                Image.network(
+                  getIcons(icon),
+                  color: Colors.yellow,
+                  // width: device.width / 5.9,
+                ),
+                Text(
+                  "$description",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
           )
         ],
-      ),
+      ),*/
     ),
     width: device.width,
-    height: device.height / 4,
+    height: device.height / 2,
     decoration: BoxDecoration(
       boxShadow: [
         BoxShadow(
@@ -53,7 +93,8 @@ Container buildCardInfo(
   );
 }
 
-Column buildLeftSectionCard(dynamic temp, dynamic feelsLike, BuildContext context) {
+Column buildLeftSectionCard(
+    dynamic temp, dynamic feelsLike, BuildContext context) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
@@ -62,7 +103,7 @@ Column buildLeftSectionCard(dynamic temp, dynamic feelsLike, BuildContext contex
         children: [
           Text(
             "${temp.toInt()}",
-            style: TextStyle(fontSize: 80, color: Color(0xff00110C)),
+            style: TextStyle(fontSize: 75, color: Color(0xff00110C)),
           ),
           SizedBox(width: 5),
           Padding(
@@ -79,9 +120,12 @@ Column buildLeftSectionCard(dynamic temp, dynamic feelsLike, BuildContext contex
           )
         ],
       ),
+      SizedBox(
+        height: 7,
+      ),
       Text(
         "${AppLocalizations.of(context).translate("feels_like")}: ${feelsLike.toInt()}",
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
       )
     ],
   );
